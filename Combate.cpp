b2400a431079b074c87a87d40935f6993c8d633c
3rick30090 <erickbu2007@gmail.com>
2026-06-09 01:23:04 -0600

    Update Combate.cpp

--- Combate.cpp
+++ Combate.cpp
@@ -10,18 +10,66 @@ void Combate::iniciarPelea(Aliado aliados[],int cantidadAliados,Enemigo &enemigo
 
         cout << "\n TURNO " << turnos << " \n";
 
-        for(int i = 0; i < cantidadAliados; i++) {
+for(int i = 0; i < cantidadAliados; i++)
+{
+    if(aliados[i].estaVivo())
+    {
+        cout << "\nTurno de " << aliados[i].getNombre() << endl;
 
-            if(aliados[i].estaVivo()) {
+        cout << "Vida: " << aliados[i].getVida() << endl;
+
+        cout << "Emocion: " << aliados[i].getEmocion() << endl;
+
+        cout << "\n1. Atacar\n";
+        cout << "2. Guardia\n";
+        cout << "3. Habilidad\n";
+
+        int opcion;
+        cin >> opcion;
+
+        switch(opcion)
+        {
+            case 1:
+
                 aliados[i].atacar(enemigo);
-            }
 
-            if(!enemigo.estaVivo()) {
-                cout << "\n Ganaste! \n";
-                estado = false;
                 break;
-            }
+
+            case 2:
+
+                aliados[i].activarGuardia();
+
+                break;
+
+            case 3:
+
+                cout << "\nSelecciona objetivo\n";
+
+                for(int j = 0; j < cantidadAliados; j++)
+                {
+                    cout << j + 1 << ". " << aliados[j].getNombre() << endl;
+                }
+
+                int objetivo;
+
+                cin >> objetivo;
+
+                aliados[i].habilidad(aliados[objetivo - 1]);
+
+                break;
+
+            default:
+
+                aliados[i].atacar(enemigo);
         }
-        turnos++;
+
+        if(enemigo.estaVivo() == false)
+        {
+            cout << "\nGANASTE!\n";
+
+            estado = false;
+
+            break;
+        }
     }
 }
