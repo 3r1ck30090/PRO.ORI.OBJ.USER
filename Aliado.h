ef01c695725ebd4674fc15b7394d7ed45dad2b43
3rick30090 <erickbu2007@gmail.com>
2026-06-09 01:02:21 -0600

    Update Aliado.h

--- Aliado.h
+++ Aliado.h
@@ -5,10 +5,16 @@
 
 class Aliado : public Personaje {
 
+private:
+    string tipo;
+
 public:
-    Aliado(string n, int v, int a, string e);
+    Aliado(string n, int v, int a, string e, string t);
 
     void atacar(Personaje &objetivo);
+
+    void habilidad(Personaje &objetivo);
+
 };
 
 #endif
