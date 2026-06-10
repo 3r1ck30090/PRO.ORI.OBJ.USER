4cca6ea7ee34172e3bc6c48112e9170b81c80198
3rick30090 <erickbu2007@gmail.com>
2026-06-10 03:58:03 -0600

    Update Aliado.cpp

--- Aliado.cpp
+++ Aliado.cpp
@@ -1,18 +1,24 @@
 #include "Aliado.h"
-
+/*
+ * Inicializa un aliado con sus atributos base.
+ */
 Aliado::Aliado(string n, int v, int a, string e, string t)
 : Personaje(n, v, a, e)
 {
     tipo = t;
 }
-
+/*
+ * Realiza un ataque con danio aumentado.
+ */
 void Aliado::atacar(Personaje &objetivo)
 {
     cout << nombre << " ataca!\n";
 
     objetivo.recibirDanio(ataque + 5);
 }
-
+/*
+ * Ejecuta la habilidad especial del personaje.
+ */
 void Aliado::habilidad(Personaje &objetivo)
 {
     if(tipo == "Omori")
