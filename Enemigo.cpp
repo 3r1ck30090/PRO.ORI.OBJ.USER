77d9248c553f20255bb3b52542f088dee256e201
3rick30090 <erickbu2007@gmail.com>
2026-06-10 04:59:03 -0600

    Update Enemigo.cpp

--- Enemigo.cpp
+++ Enemigo.cpp
@@ -5,7 +5,9 @@
 Enemigo::Enemigo(string n, int v, int a, string e)
 : Personaje(n, v, a, e) {
 }
-
+/*
+ * Ataca con un danio mayor al de los aliados.
+ */
 void Enemigo::atacar(Personaje &objetivo) {
 
     cout << nombre << " ataca agresivamente nojao >:( !\n";
